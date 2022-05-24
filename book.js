cff25449bcf6910ba42b9f61d75d5c360052405a
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-05-24 19:51:38 -0300

    added functionality for the scripts
    - Added bootstrap for the table styles

---
+++ book.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
+
+class Books {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
--- main.js
+++ main.js
@@ -1,68 +1,67 @@
-
-
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
 
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
 const addBookButton = document.getElementById('add-book');
-const BookInfo = document.getElementById('book-Info');
+const BookInfo = document.getElementById('booksContainer');
 let getmylibrary = JSON.parse(localStorage.getItem('mesLivres1'));
 
 if (JSON.parse(localStorage.getItem('mesLivres1')) === null) {
-    getmylibrary = [];
-}else{
-    getmylibrary =  JSON.parse(localStorage.getItem('mesLivres1'));
-    console.log(getmylibrary)
+  getmylibrary = [];
+} else {
+  getmylibrary = JSON.parse(localStorage.getItem('mesLivres1'));
 }
 
 function renderBook() {
-    BookInfo.innerHTML = '';
-    getmylibrary.forEach((element, index) => {
-      BookInfo.innerHTML += `<h4 >${element.title}</h4><br>
-      <h4 >${element.author}</h4>
-      <button onclick = BL.deleteBook(${index})>Remove</button>
-      <hr>`;
-    });
+  BookInfo.innerHTML = '';
+  getmylibrary.forEach((element, index) => {
+    BookInfo.innerHTML += `<tr>
+      <td class="ms-4 fs-5">
+        <span class="ms-3">
+          "${element.title}" by ${element.author}
+        </span>
+      </td>
+      <td class="d-flex">
+        <button class="btn btn-warning fs-5 mx-auto" onclick = BL.deleteBook(${index})>
+          Delete
+        </button>
+      </td>
+    </tr>`;
+  });
+}
+
+class StorageFun {
+  constructor(array) {
+    this.array = array;
   }
 
-
-class books{
-    constructor(title, author){
-        this.title = title;
-        this.author = author;
-    }
-} 
-
-class storageFun{
-    constructor(array){
-        this.array = array
-    };
     updateStorage = () => {
-        localStorage.mesLivres1 = JSON.stringify(this.array);
+      localStorage.mesLivres1 = JSON.stringify(this.array);
     };
 
     addBook = () => {
-     const newBook = new books(titleInput.value, authorInput.value);
-     this.array.push(newBook);
-     this.updateStorage();
-     renderBook();
-     title.value = '';
-     author.value = '';
-     console.log(this.array);
-   };
+      const newBook = new Books(titleInput.value, authorInput.value);
+      this.array.push(newBook);
+      this.updateStorage();
+      renderBook();
+      title.value = '';
+      author.value = '';
+    };
 
    deleteBook = (i) => {
-    this.array.splice(i, 1);
-    this.updateStorage();
-    renderBook();
-  };
-
+     this.array.splice(i, 1);
+     this.updateStorage();
+     renderBook();
+   };
 }
 
-const BL = new storageFun(getmylibrary);
+const BL = new StorageFun(getmylibrary);
 addBookButton.addEventListener('click', () => {
-    BL.addBook()
-  });
-  
-  window.addEventListener('load', () => {
-    renderBook();
-  });
+  BL.addBook();
+});
+
+window.addEventListener('load', () => {
+  renderBook();
+});
