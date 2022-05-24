49bb9acc90547556a8ddfdc3b9b2092fa11986ab
masangana <alexmasangana@gmail.com>
2022-05-24 19:52:12 +0300

    solve save and data persistance
    i added a table in loadding

--- scripts.js
+++ scripts.js
@@ -1,62 +1,51 @@
+const awesomeBooks = [];
+
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
-
-console.log(titleInput)
-
 
 const addBookButton = document.getElementById('add-book');
 
 let BookInfo = document.getElementById('book-Info');
-const awesomeBooks = [];
+const getmylibrary = JSON.parse(localStorage.getItem('saveBooks'));
 
+console.log(getmylibrary)
 
 function addBook(title, author) {
   const newBook = new Object();
   newBook.title = title;
   newBook.author = author;
-  console.log(newBook);
-  awesomeBooks.push(newBook);
-  console.log(awesomeBooks);
+  getmylibrary.push(newBook);
+  console.log(awesomeBooks)
+  showBooks(awesomeBooks);
+
+  localStorage.setItem('saveBooks', JSON.stringify(getmylibrary));
   
 }
 
 
-function showBooks(bookArray){
+function showBooks(table){
   BookInfo.innerHTML='';
-  bookArray.forEach((element,index) => {
+  getmylibrary.forEach((element,index) => {
     BookInfo.innerHTML += `<h4 >${element.title}</h4><br>
     <h4 >${element.author}</h4>
     <button onclick = removeBook(${index})>DEL</button>
     <hr>`;
   });
+  //localStorage.setItem('saveBooks', JSON.stringify(getmylibrary));
 }
 
-function removeBook (index) {
-  console.log(index)
+function removeBook(index){
+  
   awesomeBooks.splice(index, 1);
-  showBooks(awesomeBooks);
   console.log(awesomeBooks);
-  //localStorage.setItem('booksData', JSON.stringify(awesomeBooks));
+  showBooks(awesomeBooks);
 }
 
 addBookButton.addEventListener('click', () => {
   addBook(titleInput.value, authorInput.value);
-  showBooks(awesomeBooks);
-  localStorage.setItem('booksData', JSON.stringify(awesomeBooks));
 });
 
-if (JSON.parse(localStorage.getItem('booksData')) === null) {
-  console.log('empty data')
-} else {
-  let arrayData = JSON.parse(localStorage.getItem('booksData'));
-  showBooks(arrayData);
-  console.log(arrayData)
-}
+window.addEventListener('load', () => {
+  showBooks(awesomeBooks);
+});
 
-// addBook('fdsa00', 'fdsafdas');
-// addBook('book2', 'fdsafdas');
-// addBook('book3', 'fdsafdas');
-
-// removeBook(0);
-
-// showBooks(awesomeBooks);
