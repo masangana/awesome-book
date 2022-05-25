5a145683e9a60356d019430dac1597b71bbf9a75
masangana <alexmasangana@gmail.com>
2022-05-25 18:07:55 +0300

    Add html on the page and section

---
+++ main.js
@@ -0,0 +1,85 @@
+/* eslint-disable max-classes-per-file */
+
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const addBookButton = document.getElementById('add-book');
+const BookInfo = document.getElementById('booksContainer');
+const titreSect1 = document.getElementById('title-sec1');
+let getmylibrary = JSON.parse(localStorage.getItem('mesLivres1'));
+
+if (JSON.parse(localStorage.getItem('mesLivres1')) === null) {
+  getmylibrary = [];
+} else {
+  getmylibrary = JSON.parse(localStorage.getItem('mesLivres1'));
+}
+
+function renderBook() {
+  BookInfo.innerHTML = '';
+  getmylibrary.forEach((element, index) => {
+    BookInfo.innerHTML += `<tr>
+      <td class="ms-4 fs-5">
+        <span class="ms-3">
+          "${element.title}" by ${element.author}
+        </span>
+      </td>
+      <td class="d-flex">
+        <button class="btn btn-warning fs-5 mx-auto" onclick = BL.deleteBook(${index})>
+          Delete
+        </button>
+      </td>
+    </tr>`;
+  });
+
+  if (getmylibrary.length === 0) {
+    titreSect1.innerHTML = '';
+    titreSect1.innerHTML += 'there are no books yet but they will soon appear here';
+  } else {
+    titreSect1.innerHTML = '';
+    titreSect1.innerHTML += 'All awesome books';
+  }
+}
+
+class Books {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+class StorageFun {
+  constructor(array) {
+    this.array = array;
+  }
+
+    updateStorage = () => {
+      localStorage.mesLivres1 = JSON.stringify(this.array);
+    };
+
+    addBook = () => {
+      const newBook = new Books(titleInput.value, authorInput.value);
+      this.array.push(newBook);
+      this.updateStorage();
+      renderBook();
+      titleInput.value = '';
+      authorInput.value = '';
+    };
+
+   deleteBook = (i) => {
+     this.array.splice(i, 1);
+     this.updateStorage();
+     renderBook();
+   };
+}
+
+/** navigation function */
+
+/* End function */
+
+const BL = new StorageFun(getmylibrary);
+addBookButton.addEventListener('click', () => {
+  BL.addBook();
+});
+
+window.addEventListener('load', () => {
+  renderBook();
+});
